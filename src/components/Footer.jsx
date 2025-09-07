8c08f05b52c7c2b969c50231a31178c3b8908996
sudharshin <134125029+sudharshin@users.noreply.github.com>
2025-09-07 11:28:54 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FaWhatsapp, FaInstagram, FaFacebookF } from "react-icons/fa";
 
+const paragraphStyle = {
+  fontFamily: "Poppins, sans-serif",
+  fontWeight: 300,
+  fontSize: "19px",
+  lineHeight: "35px",
+  letterSpacing: "0%",
+  color: "#fff",
+};
+
 const Footer = () => {
   return (
     <footer
@@ -20,8 +29,9 @@ const Footer = () => {
           {/* Company Info */}
           <Col xs={12} sm={6} md={3}>
             <h4 style={{ fontWeight: 600, fontSize: 24 }}>Opor</h4>
-            <p style={{ fontSize: 16, fontWeight: 300, lineHeight: 1.9, color: "#fff", marginTop: 16 }}>
-              Connecting you to trusted suppliers with quality products, easy details, and quick enquiries – all in one place.
+            <p style={{ ...paragraphStyle, marginTop: 16 }}>
+              Connecting you to trusted suppliers with quality products, easy
+              details, and quick enquiries – all in one place.
             </p>
             <div className="d-flex justify-content-center justify-content-md-start gap-3 mt-4">
               {[FaWhatsapp, FaInstagram, FaFacebookF].map((Icon, idx) => (
@@ -49,11 +59,33 @@ const Footer = () => {
 
           {/* Quick Links */}
           <Col xs={12} sm={6} md={3}>
-            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>Quick Links</h6>
-            <ul className="list-unstyled" style={{ fontSize: 16, lineHeight: 2.2, paddingLeft: 0 }}>
-              {["Home", "About Us", "Products", "Enquiry Form", "Testimonials", "FAQs", "Contact Us"].map((item, i) => (
+            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>
+              Quick Links
+            </h6>
+            <ul
+              className="list-unstyled"
+              style={{ fontSize: 16, lineHeight: 2.2, paddingLeft: 0 }}
+            >
+              {[
+                "Home",
+                "About Us",
+                "Products",
+                "Enquiry Form",
+                "Testimonials",
+                "FAQs",
+                "Contact Us",
+              ].map((item, i) => (
                 <li key={i}>
-                  <a href="#" style={{ color: "#fff", textDecoration: "none", fontWeight: 500 }}>{item}</a>
+                  <a
+                    href="#"
+                    style={{
+                      color: "#fff",
+                      textDecoration: "none",
+                      fontWeight: 500,
+                    }}
+                  >
+                    {item}
+                  </a>
                 </li>
               ))}
             </ul>
@@ -61,23 +93,41 @@ const Footer = () => {
 
           {/* Products */}
           <Col xs={12} sm={6} md={3}>
-            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>Products</h6>
-            <ul className="list-unstyled" style={{ fontSize: 16, lineHeight: 2.2, paddingLeft: 0 }}>
-              {["Home", "About Us", "Products", "Enquiry Form"].map((item, i) => (
-                <li key={i}>
-                  <a href="#" style={{ color: "#fff", textDecoration: "none", fontWeight: 500 }}>{item}</a>
-                </li>
-              ))}
+            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>
+              Products
+            </h6>
+            <ul
+              className="list-unstyled"
+              style={{ fontSize: 16, lineHeight: 2.2, paddingLeft: 0 }}
+            >
+              {["Home", "About Us", "Products", "Enquiry Form"].map(
+                (item, i) => (
+                  <li key={i}>
+                    <a
+                      href="#"
+                      style={{
+                        color: "#fff",
+                        textDecoration: "none",
+                        fontWeight: 500,
+                      }}
+                    >
+                      {item}
+                    </a>
+                  </li>
+                )
+              )}
             </ul>
           </Col>
 
           {/* Address */}
           <Col xs={12} sm={6} md={3}>
-            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>Address</h6>
-            <p style={{ fontSize: 16, fontWeight: 300, marginBottom: 4 }}>
+            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 20 }}>
+              Address
+            </h6>
+            <p style={{ ...paragraphStyle, marginBottom: 4 }}>
               45 / 5, Bharathiyar 5th street
             </p>
-            <p style={{ fontSize: 16, fontWeight: 300, marginBottom: 4 }}>
+            <p style={{ ...paragraphStyle, marginBottom: 4 }}>
               Sankarankovil, Tenkasi
             </p>
             <a
@@ -94,28 +144,67 @@ const Footer = () => {
               See on Google Map
             </a>
 
-            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 8 }}>Office Hours</h6>
-            <p style={{ fontSize: 16, fontWeight: 500 }}>
+            <h6 style={{ fontWeight: 600, fontSize: 18, marginBottom: 8 }}>
+              Office Hours
+            </h6>
+            <p style={{ ...paragraphStyle, fontWeight: 500 }}>
               Mon – Fri ( <strong>8AM – 6PM</strong> )
             </p>
           </Col>
         </Row>
 
-        <hr style={{ borderColor: "rgba(255,255,255,0.15)", margin: "40px 0 30px" }} />
+        <hr
+          style={{
+            borderColor: "rgba(255,255,255,0.15)",
+            margin: "40px 0 30px",
+          }}
+        />
 
         {/* Bottom Footer */}
         <Row className="align-items-center text-center text-md-start">
           <Col xs={12} md={6}>
-            <p style={{ fontSize: 15, fontWeight: 600, marginBottom: 0 }}>
+            <p style={{ ...paragraphStyle, fontWeight: 600, fontSize: 15, marginBottom: 0 }}>
               Copyright 2025 all rights reserved
             </p>
           </Col>
           <Col xs={12} md={6} className="mt-2 mt-md-0">
             <div className="d-flex flex-wrap justify-content-center justify-content-md-end gap-4">
-              <a href="#" style={{ color: "#fff", textDecoration: "none", fontSize: 15, fontWeight: 600 }}>Privacy</a>
-              <a href="#" style={{ color: "#fff", textDecoration: "none", fontSize: 15, fontWeight: 600 }}>Security</a>
-              <a href="#" style={{ color: "#fff", textDecoration: "none", fontSize: 15, fontWeight: 600 }}>Terms</a>
-              <span style={{ fontSize: 15, fontWeight: 600 }}>Made with examplename</span>
+              <a
+                href="#"
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: 15,
+                  fontWeight: 600,
+                }}
+              >
+                Privacy
+              </a>
+              <a
+                href="#"
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: 15,
+                  fontWeight: 600,
+                }}
+              >
+                Security
+              </a>
+              <a
+                href="#"
+                style={{
+                  color: "#fff",
+                  textDecoration: "none",
+                  fontSize: 15,
+                  fontWeight: 600,
+                }}
+              >
+                Terms
+              </a>
+              <span style={{ fontSize: 15, fontWeight: 600 }}>
+                Made with examplename
+              </span>
             </div>
           </Col>
         </Row>
